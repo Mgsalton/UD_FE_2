30d20e51d33686574c110e11470ac23932815d5d
MGSA <mgsalton@gmail.com>
2019-05-26 16:32:01 +0100

    Successfully implemented: - cleaned up code - added in comments

--- js/app.js
+++ js/app.js
@@ -1,26 +1,12 @@
+/******************************************************************************
+* Welcome to the program! What follows is hours of my life... Be gentle!
+* And with that established... scroll down to the bottom for the start point
+/*****************************************************************************
 /*
- * Create a list that holds all of your cards
- */
-
-let cardList = [
-    "fa-diamond",
-    "fa-paper-plane-o",
-    "fa-anchor",
-    "fa-bolt",
-    "fa-cube",
-    "fa-anchor",
-    "fa-leaf",
-    "fa-bicycle",
-    "fa-diamond",
-    "fa-bomb",
-    "fa-leaf",
-    "fa-bomb",
-    "fa-bolt",
-    "fa-bicycle",
-    "fa-paper-plane-o",
-    "fa-cube"
-];
-
+*******************************************************************************
+* Variable declaration for global scope (and because I like C...)
+*******************************************************************************
+*/
 let newCardList = "";
 let cardOne = "";
 let cardTwo = "";
@@ -42,55 +28,41 @@ let minutes = 0;
 let hours = 0;
 let timer = 0;
 let starScore = "";
-//let firstCard = [];
-
-function generateCard(cards, deck) {
-    for (let card of cards) {
-        let newCards =`
-        <li class="card">
-            <i class="fa ${card}"></i>
-        </li>`;
-        deck.insertAdjacentHTML('afterbegin', newCards);
-    }
-};
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-};
+let cardList = [
+    "fa-diamond",
+    "fa-paper-plane-o",
+    "fa-anchor",
+    "fa-bolt",
+    "fa-cube",
+    "fa-anchor",
+    "fa-leaf",
+    "fa-bicycle",
+    "fa-diamond",
+    "fa-bomb",
+    "fa-leaf",
+    "fa-bomb",
+    "fa-bolt",
+    "fa-bicycle",
+    "fa-paper-plane-o",
+    "fa-cube"
+];
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+*******************************************************************************
+* Seventeenth function: restarts the session (called from the fourth function)
+*******************************************************************************
+*/
 
 function restart() {
     console.log('Hit restart');
     location.reload();
 };
+
+/*
+*******************************************************************************
+* Sixteenth function: populates the modal with the session details
+*******************************************************************************
+*/
 
 function populateModal(modal) {
 
@@ -101,21 +73,42 @@ function populateModal(modal) {
     <h2>${hours} hours, ${minutes} mins, and ${seconds} seconds</h2>
     <h2>In ${clickCount} moves</h2>
     <h2>Giving you an overall star rating of:</h2>
-    <h2> class="outputStars">${starScore}</h2>`;
+    <h2 class="outputStars">${starScore}</h2>`;
     modal.insertAdjacentHTML('afterbegin', modalText);
 };
+
+/*
+*******************************************************************************
+* Fifteenth function: clears the timer
+*******************************************************************************
+*/
 
 function endtimer() {
     clearInterval(timer);
 };
 
+/*
+*******************************************************************************
+* Fourteenth function: this is inevitable.
+* - it calls the endtimer function to stop the clock
+* - it then proceeds to call the populateModal function to enter session details
+* - and after a nice little finger snap, the modal is displayed...or is it?
+*******************************************************************************
+*/
+
 function endGame() {
-    //window.alert('A winner is you!');
-    //modalSelector.style.visibility ="visible";
     endtimer();
     populateModal(modalContentSelector);
     modalSelector.classList.toggle("displayModal");
 };
+
+/*
+*******************************************************************************
+* Thirteenth function:
+* - in the event of a no match, the card's classes are set to 'open show nomatch'
+* - the cleanUpFlag is set to 1 so that the code resets the classes on the next click
+*******************************************************************************
+*/
 
 function noMatch() {
     cardOne.srcElement.classList.remove('match');
@@ -123,12 +116,20 @@ function noMatch() {
     cardTwo.srcElement.classList.add('open', 'show', 'nomatch');
     cleanUpFlag = 1;
     cardHolder = [];
-    console.log('Not a match!');
+    //console.log('Not a match!');
 };
+
+/*
+*******************************************************************************
+* Twelfth function:
+* - if 8 cards match, the game ends by calling the endGame function
+* - otherwise, both matched cards have their classes set to 'open show match'
+*******************************************************************************
+*/
 
 function cardsMatch() {
     if (winCondition != 8) {
-        console.log('Match! ' + winCondition);
+        //console.log('Match! ' + winCondition);
         cardOne.srcElement.classList.add('match');
         cardTwo.srcElement.classList.add('open', 'show', 'match');
         cardHolder = [];
@@ -140,51 +141,84 @@ function cardsMatch() {
     };
 };
 
+/*
+*******************************************************************************
+* Eleventh function:
+* - the icons of cardOne and cardTwo are placed in the cardHolder array
+* - if they match, the winCondition variable is incremented by 1
+* - the cardsMatch function is then called
+*
+* - if the icons don't mach, noMatch is called
+*******************************************************************************
+*/
+
 function compareCards() {
     if (cardHolder[0] == cardHolder[1]) {
         winCondition += 1;
         cardsMatch();
     } else {
-        //cardHolder.length = 1;
-        //cardHolder.pop();
         noMatch();
     };
 };
 
-// use console.log(event) to see all the characteristics of an object
+/*
+*******************************************************************************
+* Tenth function:
+* - the first click event is added to cardOne.
+* - the class for cardOne is then switched to 'open' and 'show'
+* - the icon associated with the event (innerHTML) is pushed to cardHolder
+*
+* - to avoid the first card being clicked twice, flow control is introduced
+* - if the class of the second click is 'card', the event is set to cardTwo
+* - the innerHTML of CardTwo is added to the cardHolder array
+* - finally, the comparecards() function is used to determine if the icons match
+*******************************************************************************
+*/
+
 function updateClass(event) {
-    //detects whether array is empty
     if (cardHolder.length == 0) {
         cardOne = event;
         cardOne.srcElement.classList.add('open', 'show');
         cardHolder.push(cardOne.target.innerHTML);
-        console.log(cardOne);
+        //console.log(cardOne);
     } else if (event.srcElement.className == "card") {
         cardTwo = event;
         cardHolder.push(cardTwo.target.innerHTML);
         compareCards();
     } else {
         return;
+        // console.log(event) allows us to see the characteristics of an object
     };
 };
+
+/*
+*******************************************************************************
+* Ninth function: removes stars based on the number of clicks registered
+*******************************************************************************
+*/
 
 function updateStars() {
     if (clickCount < 32) {
-        console.log("don't delete a star now");
+        //console.log("don't delete a star now");
     } else if (clickCount == 32) {
         starSelector.removeChild(starSelector.childNodes[1]);
-        console.log("delete a star now");
+        //console.log("delete a star now");
     } else if (clickCount == 64) {
         starSelector.removeChild(starSelector.childNodes[2]);
-        console.log("delete a star now");
+        //console.log("delete a star now");
     } else if (clickCount == 88) {
         starSelector.removeChild(starSelector.childNodes[3]);
-        console.log("delete a star now");
+        //console.log("delete a star now");
     };
 };
 
+/*
+*******************************************************************************
+* Eighth function: starts the timer once a click is registered
+*******************************************************************************
+*/
+
 function startTimer() {
-    // credit: http://logicalmoon.com/2015/05/using-javascript-to-create-a-timer/
     if (clickCount == 1) {
         timer = setInterval(function() {
             seconds ++;
@@ -194,13 +228,27 @@ function startTimer() {
         }, 1000);
     } else if (clickCount > 1) {
         return;
+        // credit: http://logicalmoon.com/2015/05/using-javascript-to-create-a-timer/
     };
 };
+
+/*
+*******************************************************************************
+* Seventh function: logs the number of clicks
+*******************************************************************************
+*/
 
 function updateMoves() {
     clickCount += 1;
     moveCountSelector.innerHTML = clickCount;
 };
+
+/*
+*******************************************************************************
+* Sixth function: makes sure that the clicked card isn't already flipped
+* - the cleanUpFlag is set at the thirteenth function
+*******************************************************************************
+*/
 
 function cleanUp() {
     if (cleanUpFlag == 1) {
@@ -212,7 +260,12 @@ function cleanUp() {
     };
 };
 
-// with thanks to: https://davidwalsh.name/event-delegate
+/*
+*******************************************************************************
+* Fifth function: executed once a card has been clicked once
+*******************************************************************************
+*/
+
 function cardClicked(event) {
     if (event.target && event.target.nodeName == "LI") {
         cleanUp();
@@ -220,14 +273,62 @@ function cardClicked(event) {
         startTimer();
         updateStars();
         updateClass(event);
-        //console.log(event);
+        // with thanks to: https://davidwalsh.name/event-delegate
     };
 };
+
+/*
+*******************************************************************************
+* Fourth function: once the deck has been created, we listen for clicks
+*******************************************************************************
+*/
 
 function runningProgram() {
     deckSelector.addEventListener('click', cardClicked);
     restartSelector.addEventListener('click', restart);
 };
+
+/*
+*******************************************************************************
+* Third function: generates HTML cards from shuffled deck
+*******************************************************************************
+*/
+
+function generateCard(cards, deck) {
+    for (let card of cards) {
+        let newCards =`
+        <li class="card">
+            <i class="fa ${card}"></i>
+        </li>`;
+        deck.insertAdjacentHTML('afterbegin', newCards);
+    };
+};
+
+/*
+*******************************************************************************
+* Second function: shuffles cards and returns array
+*******************************************************************************
+*/
+
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        // Shuffle function from http://stackoverflow.com/a/2450976
+    };
+    return array;
+};
+
+/*
+*******************************************************************************
+* First function: takes existing cards and passes them to the shuffle function
+*******************************************************************************
+*/
 
 function startPoint() {
     cardList = shuffle(cardList);
@@ -235,9 +336,11 @@ function startPoint() {
     return null;
 };
 
+/*
+*******************************************************************************
+* Start of program
+*******************************************************************************
+*/
+
 startPoint();
 runningProgram();
-
-
-//console.log("You win!");
-// while there aren't 8 matches, do this code
